1a8897d483f411ce412fcbb187f17848529edb4a
Manish <mnagar874@gmail.com>
2018-11-08 06:04:22 +0530

    initial

---
+++ app/src/main/java/com/nagar/manish/fitnessapp/FourthFragment.java
@@ -0,0 +1,67 @@
+package com.nagar.manish.fitnessapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class FourthFragment extends Fragment {
+
+    View view;
+    ListView listview;
+    static int b;
+    String[]homeRemediesTips =
+            {
+                    "Acidity",
+                    "Cough and Cold",
+                    "Back Pain",
+                    "Pimple Care",
+                    "Tooth Ache",
+                    "Weight Loss",
+                    "Hair Loss",
+                     "Skin Rashes",
+                    "Asthma",
+                    "Mouth Ulcer",
+                    "Sore Throat"
+            };
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_fourth, container, false);
+        // get the reference of Button
+        listview = (ListView) view.findViewById(R.id.l1);
+        // perform setOnClickListener on first Button
+        ArrayAdapter<String> fu=new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,homeRemediesTips);
+        listview.setAdapter(fu);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i<9)
+                {
+                    b=i;
+                    Intent A=new Intent(getActivity(),Detailshow.class);
+                    startActivity(A);
+                }
+                else
+                {
+                    b=i;
+                    Intent A=new Intent(getActivity(),WebActivity.class);
+                    startActivity(A);
+                }
+
+            }
+        });
+        return view;
+
+    }
+}
